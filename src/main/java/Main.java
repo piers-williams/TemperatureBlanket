646b02684a1f3a1327b8dbd6ea9f07fc530db59a
Piers Williams <piers.williams@red-gate.com>
2022-01-04 08:24:23 +0000

    Add support for squares with inners for min

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,13 +11,22 @@ public class Main {
 
         var temperatureData = Temperatures.readTemperatures(temperatureFileName);
         var scaleData = TemperatureScale.getColors(scaleFileName);
-final int height = 30;
-        var image = new BufferedImage(temperatureData.length, height, BufferedImage.TYPE_INT_RGB );
+        final int squareHeight = 30;
+        var width = 18;
+        var height = 21;
+        var image = new BufferedImage(width * squareHeight, height * squareHeight, BufferedImage.TYPE_INT_RGB );
         var graphics = image.getGraphics();
-        for(int i = 0; i < temperatureData.length; i++){
-            graphics.setColor(scaleData.get(Math.round(temperatureData[i])));
+
+        for(int i = 0; i < width; i++){
             for(int j = 0; j < height; j++){
-                graphics.drawRect(i, j, 1, 1);
+                var temps = temperatureData[i * width + j];
+                graphics.setColor(scaleData.get(Math.round(temps[0])));
+                graphics.fillRect(i * squareHeight, j * squareHeight, squareHeight, squareHeight);
+                graphics.setColor(scaleData.get(Math.round(temps[1])));
+                graphics.fillRect((i * squareHeight) + squareHeight / 4,
+                        j * squareHeight + squareHeight / 4,
+                        squareHeight / 2,
+                        squareHeight / 2);
             }
         }
 
--- src/main/java/Temperatures.java
+++ src/main/java/Temperatures.java
@@ -5,13 +5,12 @@ import java.util.List;
 
 public class Temperatures {
 
-    public static Float[] readTemperatures(String fileName) throws IOException {
+    public static Float[][] readTemperatures(String fileName) throws IOException {
         List<String> lines = Files.readAllLines(Path.of(fileName));
         return lines.stream()
                 .skip(1)
                 .map(x -> x.split(","))
-                .map(x -> x[1])
-                .map(Float::parseFloat)
-                .toArray(Float[]::new);
+                .map(x -> new Float[]{Float.parseFloat(x[1]), Float.parseFloat(x[3])})
+                .toArray(Float[][]::new);
     }
 }
