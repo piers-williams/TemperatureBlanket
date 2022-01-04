ab431f800970b822d070f301cc56a33d5334d54f
Piers Williams <piers.williams@red-gate.com>
2022-01-04 08:38:47 +0000

    Not sure what I'm doing now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,19 +14,22 @@ public class Main {
         final int squareHeight = 30;
         var width = 18;
         var height = 21;
-        var image = new BufferedImage(width * squareHeight, height * squareHeight, BufferedImage.TYPE_INT_RGB );
+        var image = new BufferedImage((width + 2) * squareHeight, (height + 2) * squareHeight, BufferedImage.TYPE_INT_RGB );
         var graphics = image.getGraphics();
 
-        for(int i = 0; i < width; i++){
-            for(int j = 0; j < height; j++){
-                var temps = temperatureData[i * width + j];
-                graphics.setColor(scaleData.get(Math.round(temps[0])));
-                graphics.fillRect(i * squareHeight, j * squareHeight, squareHeight, squareHeight);
-                graphics.setColor(scaleData.get(Math.round(temps[1])));
-                graphics.fillRect((i * squareHeight) + squareHeight / 4,
-                        j * squareHeight + squareHeight / 4,
-                        squareHeight / 2,
-                        squareHeight / 2);
+        for(int i = 0; i <= width; i++){
+            for(int j = 0; j <= height; j++){
+                var index = (i * width) + j;
+                if(temperatureData.length > index) {
+                    var temps = temperatureData[index];
+                    graphics.setColor(scaleData.get(Math.round(temps[0])));
+                    graphics.fillRect(i * squareHeight, j * squareHeight, squareHeight, squareHeight);
+                    graphics.setColor(scaleData.get(Math.round(temps[1])));
+                    graphics.fillRect((i * squareHeight) + squareHeight / 4,
+                            j * squareHeight + squareHeight / 4,
+                            squareHeight / 2,
+                            squareHeight / 2);
+                }
             }
         }
 
